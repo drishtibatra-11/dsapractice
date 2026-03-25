812948a7032ab8bf6719dfe472417c329698f661
drishti batra <batradrishti22@gmail.com>
2026-03-25 10:22:41 +0530

    dp27.c

---
+++ dp27.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int x;
+
+    struct Node *head = NULL, *temp = NULL, *newNode;
+
+    // Create linked list
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+
+        // Create new node
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = x;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Traverse and print
+    temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
