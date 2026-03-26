c921910c18e4c187b40eeed2bce0ba30ccafe8f9
drishti batra <batradrishti22@gmail.com>
2026-03-26 16:51:18 +0530

    dp31.c

---
+++ dp30.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+
+#define MAX 5
+int stack[MAX];
+int TOP = -1;
+void push(int x);
+void display();
+void pop();
+
+int main()
+{
+    int x, i;
+    for(i=0; i<MAX-1; i++)
+    {
+    printf("Enter the number to be pushed: ");
+    scanf("%d", &x);
+
+    push(x);
+    }
+
+    display(); 
+    pop();
+
+    return 0;
+}
+void push(int x)
+{
+    if (TOP == MAX - 1)
+    {
+        printf("Stack Overflow\n");
+    }
+    else
+    {
+        TOP++;
+        stack[TOP] = x;
+    }
+}
+
+void display()
+{
+    if (TOP == -1)
+    {
+        printf("Stack is empty\n");
+        return;
+    }
+
+    printf("Stack elements are:\n");
+    for (int i = TOP; i >= 0; i--)
+    {
+        printf("%d\n", stack[i]);
+    }
+}
+void pop()
+{
+    if (TOP == -1)
+    {
+        printf("Stack Underflow\n");
+    }
+    else
+    {
+        int element = stack[TOP];
+        TOP--;
+        printf("Popped element: %d\n", element);
+    }
+}
---
+++ dp31.c
@@ -0,0 +1,67 @@
+//implementation using linked list
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+void push(int x);
+void pop();
+void display();
+
+
+struct Node* top = NULL;
+int main() {
+    int x, n;
+    printf("Enter the number of elements to push: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter the number to be pushed: ");
+        scanf("%d", &x);
+        push(x);
+    }
+
+    display();
+    pop();
+    pop();
+
+    return 0;
+}
+
+void push(int x) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = x;
+    newNode->next = top;
+    top = newNode;
+}
+
+void pop() {
+    if (top == NULL) {
+        printf("Stack Underflow\n");
+    } else {
+        struct Node* temp = top;
+        printf("Popped element: %d\n", temp->data);
+        top = top->next;
+        free(temp);
+    }
+}
+
+void display()
+ 
+{
+if (top==NULL)
+{
+   printf("stack empty");
+}
+   else
+   {
+    struct Node* temp = top;
+    printf("Stack elements are:\n");
+    while (temp != NULL) {
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }
+   }
+}
