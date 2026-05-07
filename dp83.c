78ecb9dd3b6a4c2a3bb56aec99a5d12a62413f37
drishti batra <batradrishti22@gmail.com>
2026-05-07 23:37:37 +0530

    dp83.c

---
+++ dp83.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure for tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Build tree from level-order array
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node** queue = (struct Node**)malloc(n * sizeof(struct Node*));
+    int front = 0, rear = 0;
+
+    struct Node* root = createNode(arr[0]);
+    queue[rear++] = root;
+
+    int i = 1;
+
+    while (front < rear && i < n) {
+        struct Node* curr = queue[front++];
+
+        // Left child
+        if (arr[i] != -1) {
+            curr->left = createNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        // Right child
+        if (i < n && arr[i] != -1) {
+            curr->right = createNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    free(queue);
+    return root;
+}
+
+// Check if two trees are mirror images
+int isMirror(struct Node* t1, struct Node* t2) {
+    if (t1 == NULL && t2 == NULL)
+        return 1;
+
+    if (t1 == NULL || t2 == NULL)
+        return 0;
+
+    return (t1->data == t2->data) &&
+           isMirror(t1->left, t2->right) &&
+           isMirror(t1->right, t2->left);
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int arr[N];
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* root = buildTree(arr, N);
+
+    if (root == NULL || isMirror(root->left, root->right))
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+    return 0;
+}
