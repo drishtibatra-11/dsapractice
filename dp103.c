f54d5c4e7855e4f002947946cdc4383ae6b248fc
drishti batra <batradrishti22@gmail.com>
2026-05-17 23:55:40 +0530

    dp103.c

---
+++ dp103.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int lowerBound(int arr[], int n, int x) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while(low <= high) {
+        int mid = (low + high) / 2;
+
+        if(arr[mid] >= x) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+int upperBound(int arr[], int n, int x) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while(low <= high) {
+        int mid = (low + high) / 2;
+
+        if(arr[mid] > x) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    int n, x;
+
+    // Input size
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input sorted array
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Input target value
+    scanf("%d", &x);
+
+    // Find bounds
+    int lb = lowerBound(arr, n, x);
+    int ub = upperBound(arr, n, x);
+
+    // Output
+    printf("%d %d", lb, ub);
+
+    return 0;
+}
