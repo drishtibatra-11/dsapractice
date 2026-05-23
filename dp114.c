8e6894c2f342b956b61c283e67a5bb3b2ce42451
drishti batra <batradrishti22@gmail.com>
2026-05-23 21:48:34 +0530

    dp114.c

---
+++ dp114.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+
+    for(i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        // Shift elements greater than key
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        // Insert key at correct position
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+
+    printf("Sorted array: ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
