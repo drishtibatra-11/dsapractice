8c1904d61e894bfe76d75c7149c2104f8d78ceb3
drishti batra <batradrishti22@gmail.com>
2026-05-27 23:35:51 +0530

    dp122.c

---
+++ dp122.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int value;
+    int index;
+} Pair;
+
+void merge(Pair arr[], int left, int mid, int right, int count[]) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    Pair L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+
+    for (int i = 0; i < n2; i++)
+        R[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+    int rightCount = 0;
+
+    while (i < n1 && j < n2) {
+        if (L[i].value <= R[j].value) {
+            count[L[i].index] += rightCount;
+            arr[k++] = L[i++];
+        } else {
+            rightCount++;
+            arr[k++] = R[j++];
+        }
+    }
+
+    while (i < n1) {
+        count[L[i].index] += rightCount;
+        arr[k++] = L[i++];
+    }
+
+    while (j < n2) {
+        arr[k++] = R[j++];
+    }
+}
+
+void mergeSort(Pair arr[], int left, int right, int count[]) {
+    if (left >= right)
+        return;
+
+    int mid = (left + right) / 2;
+
+    mergeSort(arr, left, mid, count);
+    mergeSort(arr, mid + 1, right, count);
+
+    merge(arr, left, mid, right, count);
+}
+
+int main() {
+    int nums[] = {5, 2, 6, 1};
+    int n = sizeof(nums) / sizeof(nums[0]);
+
+    Pair arr[n];
+    int count[n];
+
+    for (int i = 0; i < n; i++) {
+        arr[i].value = nums[i];
+        arr[i].index = i;
+        count[i] = 0;
+    }
+
+    mergeSort(arr, 0, n - 1, count);
+
+    printf("Counts of smaller elements on right:\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", count[i]);
+    }
+
+    return 0;
+}
